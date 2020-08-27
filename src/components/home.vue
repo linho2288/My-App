537b7d7ac04ccf9358b9ff26c80276b275da1be9
mike <mikelinho4all@gmail.com>
2020-08-27 05:46:17 +0100

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,42 @@
+<template> 
+<div>
+<h1 id='home'> home  </h1>
+
+</div>
+
+
+
+ </template>
+
+ <script>
+ export default{
+ name:'home',
+ data(){
+
+    return{
+
+    }
+ }
+ }
+
+
+
+  </script>
+
+
+
+
+  <style scoped>
+  #home{
+      
+  } 
+
+
+
+
+  </style>
+
+
+
+
+ 
